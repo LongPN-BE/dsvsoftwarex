1c8cf1bed4d5d0662dc4aa87cbc2ca25dbb7568c
Bennny20 <phientruong20@gmail.com>
2024-04-29 05:02:50 +0700

    first commit

--- index.js
+++ index.js
@@ -1,26 +1,31 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const config = require('./config');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/routes');
+const express = require("express");
+const config = require("./config");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/routes");
 const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true,
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 const app = express();
 app.use(express.json());
 app.use(cors(corsOptions));
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // API routes ==> https://localhost:8081/api/{domain api}
-app.use('/api', cors(corsOptions), userRoutes.router);
+app.use("/api", cors(corsOptions), userRoutes.router);
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger-output.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(config.port);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+// app.listen(config.port);
+
+app.listen(config.port, () => {
+  console.log("Server is running on port " + config.port);
+});
